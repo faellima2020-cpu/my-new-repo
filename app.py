29082f06cfb0cd4481284496ef53dec9f131782d
faellima2020-cpu <faellima2020@icloud.com>
2026-03-02 16:15:44 +0000

    Create scatter plot for car data
    Add scatter plot for car data based on odometer and price.

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ if hist_button:
     st.write ('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram (car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+fig.show() # exibindo
