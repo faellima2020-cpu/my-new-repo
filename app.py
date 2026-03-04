2c42df35ca647101788eb8584cbfe32304473b1d
faellima2020-cpu <faellima2020@icloud.com>
2026-03-04 07:30:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
+car_data = pd.read_csv('vehicles_us.csv')  # lendo os dados
+
 st.header('Preços de Carro')
-hist_button = st.button('Criar histograma') # criar um botão
+
+hist_button = st.button('Criar histograma')  # botão
 
 if hist_button:
-    st.write ('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.histogram (car_data, x="odometer")
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
-fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
-fig.show() # exibindo
+
+# gráfico de dispersão
+fig = px.scatter(car_data, x="odometer", y="price")
+
+st.write("Gráfico de dispersão: Quilometragem vs Preço")
+
+st.plotly_chart(fig, use_container_width=True)
